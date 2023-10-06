58d1e18afafaebadb5c3a39ceaa0deaac191cbad
Obi Enechionyia <obienechionyia@gmail.com>
2023-10-06 13:31:02 -0700

    Fix test bug

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,7 +5,10 @@ import {
   updateUser,
   getMatchStats,
 } from "../controllers/userController.js";
-import { authorizePermissions } from "../middleware/authMiddleware.js";
+import {
+  authorizePermissions,
+  checkForTestUser,
+} from "../middleware/authMiddleware.js";
 import upload from "../middleware/multerMiddleware.js";
 import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
 
@@ -13,6 +16,7 @@ router.get("/current-user", getCurrentUser);
 router.get("/admin/match-stats", authorizePermissions("admin"), getMatchStats);
 router.patch(
   "/update-user",
+  checkForTestUser,
   upload.single("avatar"),
   validateUpdateUserInput,
   updateUser
